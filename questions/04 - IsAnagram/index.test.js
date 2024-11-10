78d6a5a9a877d8d6343315a6f2b8c82a9f6e9573
Guyda <guydavid88@gmail.com>
2024-11-10 13:44:54 +0200

    isAnagram2

--- questions/04 - IsAnagram/index.test.js
+++ questions/04 - IsAnagram/index.test.js
@@ -4,4 +4,5 @@ xtest("isAnagram", () => {
   expect(isAnagram("sub", "ubs")).toBeTruthy();
   expect(isAnagram("like", "keil")).toBeTruthy();
   expect(isAnagram("cat", "taca")).toBeFalsy();
+  expect(isAnagram("aab", "abb")).toBeFalsy();
 });
