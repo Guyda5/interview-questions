6bba282fb6e6bd4fe9d7cdcb640223609220aace
Guyda <guydavid88@gmail.com>
2024-12-27 09:47:06 +0200

    nodesSum

---
+++ questions/09 - NodesSum/index.js
@@ -0,0 +1,47 @@
+const nodesSum = (input) => {};
+export { nodesSum };
+
+const sampleInput = {
+  key1: {
+    key11: {
+      key111: 10,
+      key112: {
+        key1121: 20,
+        key1122: {
+          key11221: 15,
+        },
+      },
+    },
+    key12: 5,
+  },
+  key2: 30,
+  key3: {
+    key31: {
+      key311: {
+        key3111: 8,
+        key3112: 12,
+      },
+    },
+    key32: 6,
+    key33: {
+      key331: 25,
+      key332: {
+        key3321: 10,
+        key3322: 5,
+      },
+    },
+  },
+  key4: {
+    key41: 7,
+    key42: {
+      key421: {
+        key4211: 3,
+        key4212: 9,
+      },
+    },
+    key43: 4,
+  },
+  key5: 50,
+};
+
+console.log("sum is: ", nodesSum(sampleInput)); // Should log sum is: 219;
